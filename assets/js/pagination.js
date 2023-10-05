f5c7529231fdec920626d0c5a826e537cf182b7e
shivdurgsamrajya <146632905+shivdurgsamrajya@users.noreply.github.com>
2023-10-05 16:18:23 +0530

    Update pagination.js
    Signed-off-by: shivdurgsamrajya <146632905+shivdurgsamrajya@users.noreply.github.com>

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,5 +1,5 @@
 const articles = [
-'<article class="row blog_item gmd-5">
+`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/kalavantin/kalavantin-durg-photos.jpg" alt="kalavantin-durg-photos" height="100%;">                               
                             </div>
@@ -14,8 +14,8 @@ const articles = [
                                   Kalavantin Durg, also known as Kalavantins Fort, is a formidable hill fort located in the Raigad district of Maharashtra, India. The fort is perched at an elevation of approximately 2,300 feet above sea level and is a part of the Sahyadri mountain range. It stands adjacent to another famous fort, Prabalgad, and together they offer a trekking experience like no other. Kalavantin Durg is renowned for its steep and narrow rock-cut steps that lead to the summit, making it a challenging yet rewarding trek.
                                 </p>                                
                             </div>
-                        </article>',
-			'<article class="row blog_item gmd-5">
+                        </article>`,
+			`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/harihar/harihar-trekking.jpg" alt="harihar fort trekking" height="100%;">                               
                             </div>
@@ -30,8 +30,8 @@ const articles = [
                                   Harihar Fort stands at an elevation of approximately 3,676 feet (1,120 meters) above sea level and offers a challenging yet rewarding trekking experience. It is a popular choice among adventure enthusiasts and history buffs due to its distinctive rock-cut steps, stunning views, and historical significance.
                                 </p>                                
                             </div>
-                        </article>',
-			'<article class="row blog_item gmd-5">
+                        </article>`,
+			`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/raigad/raigad-photos.jpg" alt="raigad photos" height="100%;">                               
                             </div>
@@ -46,8 +46,8 @@ const articles = [
                                   Raigad Fort, originally known as "Rairi," holds immense historical significance. It was the capital of the Maratha Empire under the visionary Chhatrapati Shivaji Maharaj. The fort stands as a testament to Indias rich heritage and the valor of its people.
                                 </p>                                
                             </div>
-                        </article>',
-			'<article class="row blog_item gmd-5">
+                        </article>`,
+			`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/harishchandragad/harishchandragad-trekking.jpg" alt="harishchandragad trekking" height="100%;">                               
                             </div>
@@ -62,8 +62,8 @@ const articles = [
                                   Harishchandragad Fort stands at an elevation of approximately 4,671 feet (1,423 meters) above sea level and offers a challenging yet rewarding trekking experience. The trek is famous for its diverse landscapes, including dense forests, steep cliffs, and scenic plateaus.
                                 </p>                                
                             </div>
-                        </article>',
-			'<article class="row blog_item gmd-5">
+                        </article>`,
+			`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/lohgad/lohgad-fort-photos.jpg" alt="lohgad trekking" height="100%;">                               
                             </div>
@@ -78,8 +78,8 @@ const articles = [
                                   Lohgad, also known as "Iron Fort," has a rich historical background dating back to the 16th century. Built by the Maratha Empire, this fort holds immense significance due to its strategic location. Lets delve deeper into the historical tapestry of Lohgad.
                                 </p>                                
                             </div>
-                        </article>',
-			'<article class="row blog_item gmd-5">
+                        </article>`,
+			`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/torana/torana-fort-photos.jpg" alt="torana fort trekking" height="100%;">                               
                             </div>
@@ -94,8 +94,8 @@ const articles = [
                                   Torana Fort, also known as Prachandagad, has a storied history dating back centuries. It is believed to have been constructed during the reign of the Maratha ruler Shivaji Maharaj. The fort played a significant role in guarding the region against potential invaders and served as a strategic outpost.
                                 </p>                                
                             </div>
-                        </article>',
-			'<article class="row blog_item gmd-5">
+                        </article>`,
+			`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/pratapgad/pratapgad-photo.jpg" alt="pratapgad fort trekking" height="100%;">                               
                             </div>
@@ -110,8 +110,8 @@ const articles = [
                                   The history of Pratapgad is intertwined with the Maratha empire. It was built by Chhatrapati Shivaji Maharaj in 1656 as a strategic fort to defend against the mighty Adilshahi Sultanate. The fort witnessed the famous Battle of Pratapgad in 1659, where Shivaji Maharaj defeated the Adilshahi commander Afzal Khan. Today, the fort stands as a symbol of Maratha valor and bravery.
                                 </p>                                
                             </div>
-                        </article>',
-			'<article class="row blog_item gmd-5">
+                        </article>`,
+			`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/rajgad/rajgad-fort-photo.jpg" alt="rajgad fort trekking" height="100%;">                               
                             </div>
@@ -126,8 +126,8 @@ const articles = [
                                   Rajgad Fort holds immense historical significance as it was the site of pivotal moments in Maratha history. It was from here that Shivaji Maharaj expanded his kingdom and laid the foundation of the Maratha Empire. The fort witnessed numerous battles and negotiations, making it a symbol of Maratha valor.
                                 </p>                                
                             </div>
-                        </article>',
-			'<article class="row blog_item gmd-5">
+                        </article>`,
+			`<article class="row blog_item gmd-5">
                             <div class="blog_item_img col-sm-6">
                                 <img class="card-img rounded-0" src="assets/img/fort/tung/tung-fort-photos.jpg" alt="tung fort trekking" height="100%;">                               
                             </div>
@@ -142,7 +142,7 @@ const articles = [
                                   Tung Fort has a rich historical background, once serving as a strategic outpost for the Maratha Empire. It played a pivotal role during various battles and is a symbol of Maratha valor.
                                 </p>                                
                             </div>
-                        </article>'
+                        </article>`
 
 ];
 
