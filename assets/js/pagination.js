5abf5de174f3859cf501b016aabd4b3c5de19d8a
shivdurgsamrajya <146632905+shivdurgsamrajya@users.noreply.github.com>
2023-10-07 19:05:12 +0530

    Update pagination.js
    Signed-off-by: shivdurgsamrajya <146632905+shivdurgsamrajya@users.noreply.github.com>

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -176,6 +176,22 @@ const articles = [
 The journey takes around 2-3 hours, offering scenic views of the Western Ghats.
                                 </p>                                
                             </div>
+                        </article>`,
+			`<article class="row blog_item gmd-5">
+                            <div class="blog_item_img col-sm-6">
+                                <img class="card-img rounded-0" src="assets/img/fort/malhargad/malhargad-fort-photos.jpg" alt="malhargad fort trekking" height="100%;">                               
+                            </div>
+                            <div class="blog_details col-sm-6">
+                                <a class="d-inline-block" href="malhargad-fort-trekking.html">
+                                    <h2 class="show-line-1">Malhargad Trekking: A Mesmerizing Escape into Nature's Lap</h2>
+                                </a>
+                                <ul class="blog-info-link">
+                                    <li><a href="#"><i class="fa fa-tags"></i> Travel, Trekking, Malhargad Fort</a></li>
+                                </ul>
+                                <p class="show-line-3">
+                                 Malhargad, also known as Sonori Fort, has a rich historical significance dating back to the Maratha Empire.
+                                </p>                                
+                            </div>
                         </article>`
 
 ];
